a4281c60084e6e828b7a2ff6d70454d06a92076f
prabhapk444 <karanprabha22668@gmail.com>
2024-11-28 09:01:56 +0530

    cm

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -54,7 +54,7 @@ export default function Hero() {
             className="flex justify-center space-x-4"
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.8, delay: 0.4 }}
+            transition={{ duration: 0.8, delay: 0.2 }}
           >
             <a
               href="#projects"
